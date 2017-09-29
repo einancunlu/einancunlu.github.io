807c902e6eab8aeb4bb6a8f8d5bd969ad5d71b9c
Emin Inanc Unlu <einancunlu@gmail.com>
2017-09-29 20:13:43 +0300

    Version 2.0.1 Fix

--- js/main.js
+++ js/main.js
@@ -5,6 +5,17 @@
 // ----------------------------------------------------------------------------
 
 
+// If Internet Explorer
+// --------------------------------------------------
+
+var ua = window.navigator.userAgent;
+var old_ie = ua.indexOf('MSIE ');
+var new_ie = ua.indexOf('Trident/');
+
+if ((old_ie > -1) || (new_ie > -1)) { // IF IE
+  $(".ie-support").css("display", "block");
+}
+
 // Google Analytics
 // --------------------------------------------------
 
